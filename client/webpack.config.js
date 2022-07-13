27cfb9a58bd843fcb5449777b6fa5316522a1bf6
Jessie Doherty <zhuzhu930@yahoo.com>
2022-07-13 18:04:25 -0500

    adding WebpackPwaManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,11 +20,29 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'J.A.T.E',
+        title: 'PWA.T.E',
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest:'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'PWA Text Editor',
+        short_name: 'PWA.T.E',
+        description: 'Take notes with code highlighting', 
+        background_color:'#08777a',
+        theme_color: '#a0d6b4',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
     ],
 
