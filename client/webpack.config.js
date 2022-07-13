e1d18a4cb1c54103d959286a7785d1fa430fd5d3
Jessie Doherty <zhuzhu930@yahoo.com>
2022-07-13 18:09:18 -0500

    adding comments to webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,6 +15,7 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Add and configure workbox plugins for a service worker and manifest file.
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'PWA.T.E',
@@ -47,6 +45,7 @@ module.exports = () => {
     ],
 
     module: {
+      // Add CSS loaders and babel to webpack.
       rules: [
         {
           test: /\.css$/i,
