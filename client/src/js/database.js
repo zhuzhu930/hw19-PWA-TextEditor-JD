496947ee1333061c058ee506710215473c21f4df
Jessie Doherty <zhuzhu930@yahoo.com>
2022-07-15 22:01:19 -0500

    editing data.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,20 +14,25 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Use PUT request to update the database');
-
+  console.log('Use PUT to update the database');
+  // Open the database
   const jateDb = await openDB('jate', 1); 
+  // Set up transaction, use readwrite to update: 
   const tx = jateDb.transaction('jate', 'readwrite');
+  // Use store method to store the data.
   const store = tx.objectStore('jate');
+  // Store.put method to save the content.
   const request = store.put({ id: 1, value: content });
+  // Awaiting the request
   const result = await request; 
 
+  // Use console.log to see whether we're able to persist the data.
   console.log('The data is saved to the database', result.value); 
-} 
+}; 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('Use GET request to get from the database.');
+  console.log('Use GET to get from the database.');
   // Open the database: 
   const jateDb = await openDB('jate', 1); 
   // Set up a transaction, use readonly to get data: 
